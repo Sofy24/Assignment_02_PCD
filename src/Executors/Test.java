86ba6fa704ae44055b40d05b8e0e5b257b7090a3
Serafino Pandolfini <serafino.pandolfini@studio.unibo.it>
2023-05-01 19:17:35 +0200

    executors getReport part 1 finished

--- src/Executors/Test.java
+++ src/Executors/Test.java
@@ -5,8 +5,12 @@ import Utilities.Report;
 public class Test {
 
     public static void main(String[] args) {
-        SourceAnalyser s = new ExecutorsSourceAnalyser();
-        Report report = s.getReport("C:\\Users\\seraf\\OneDrive\\Desktop\\SSS\\ASSIGNMENT1\\f1 (2)\\f1", 5, 5, 200);
+        String directory = args[0];
+        int longestFiles = Integer.parseInt(args[1]);
+        int numberOfRanges = Integer.parseInt(args[2]);
+        int maxLines = Integer.parseInt(args[3]);
+        Report report = new ExecutorsSourceAnalyser().getReport(directory, longestFiles, numberOfRanges, maxLines);
+        //Report report = s.getReport("C:\\Users\\seraf\\OneDrive\\Desktop\\SSS\\ASSIGNMENT1\\f1 (2)\\f1", 5, 5, 200);
         report.getResults();
     }
 }
